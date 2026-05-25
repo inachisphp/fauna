6668237ce74311583d0029d2d1c14e62b7e86644
David G. Paul <jedi58@gmail.com>
2026-05-25 14:33:01 +0100

    Updated extension config

--- src/FaunaBundle.php
+++ src/FaunaBundle.php
@@ -8,17 +8,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FaunaBundle extends Bundle
 {
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
 
-    /**
-     * Returns the bundle's container extension class name.
-     * Symfony uses this to locate the DI Extension automatically.
-     */
-    public function getContainerExtensionClass(): string
-    {
-        return \Inachis\Fauna\DependencyInjection\FaunaExtension::class;
-    }
 }
