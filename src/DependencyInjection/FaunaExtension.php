91a74c8afb8f02ba45e76a34ba249e5e805cafa6
David G. Paul <dave.paul@jadu.net>
2026-05-26 19:00:04 +0100

    Fixed dependency injection path

--- src/DependencyInjection/FaunaExtension.php
+++ src/DependencyInjection/FaunaExtension.php
@@ -13,7 +13,7 @@ class FaunaExtension extends Extension
     {
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__ . '/../Resources/config')
+            new FileLocator(__DIR__ . '/../../config')
         );
 
         $loader->load('services.yaml');
